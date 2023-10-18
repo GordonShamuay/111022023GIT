e6251db1ddbb68363c4bbfdaae945a44f460d36c
GordonShamuay <john.belikov@gmail.com>
2023-10-18 18:42:07 +0600

    Dobavili Urok 3

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа сложения 2-х чисел
+//Console.Write("Введите число 1: ");
+int nomer1 = 5;//Console.WriteLine();
+//Console.Write("Введите число 2: ");
+int nomer2 = 6;//Console.WriteLine();
+Console.Write("Сумма чисел = ");
+Console.WriteLine(nomer1 + nomer2);
